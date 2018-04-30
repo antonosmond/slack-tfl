e4d7eaccd19cf1fc585c8dcf3a155004ac5773fb
Anton Osmond <antonosmond@gmail.com>
2018-05-01 00:03:02 +0100

    update eslint config & .gitignore

--- .eslintrc.js
+++ .eslintrc.js
@@ -13,16 +13,25 @@ module.exports = {
     "arrow-spacing": "error",
     "callback-return": "error",
     "camelcase": "error",
+    "class-methods-use-this": "error",
     "comma-dangle": ["error", "always-multiline"],
     "default-case": "error",
     "indent": ["error", 2],
     "linebreak-style": ["error", "unix"],
     "no-console": "warn",
+    "no-multiple-empty-lines": "error",
     "no-template-curly-in-string": "error",
+    "no-throw-literal": "error",
     "no-var": "error",
     "object-curly-spacing": ["error", "always"],
     "prefer-arrow-callback": "error",
     "prefer-const": ["error"],
+    "prefer-destructuring": ["error", {
+      "array": true,
+      "object": true
+    }, {
+      "enforceForRenamedProperties": false
+    }],
     "prefer-template": "error",
     "quotes": ["error", "single"],
     "require-await": "error",
